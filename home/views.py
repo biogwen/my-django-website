998fef11b9aa27ed6d2446510effa222aaedc5d0
biogwen <g.mouthuy@gmail.com>
2021-06-05 16:31:53 +0200

    saturday

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
     return render(request, 'welcome.html')
+
+def cv(request):
+    args = {}
+    datum = datetime.now()
+    years = datum.year - 2007
+    args['years'] = years
+    return render(request, 'cv.html', args)
