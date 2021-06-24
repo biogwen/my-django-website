fab01704c1e555c8ad641c98e2413469c81bb59b
quiorte <g.mouthuy@gmail.com>
2021-06-24 17:34:23 +0200

    change model for quiz

--- quizz/models.py
+++ quizz/models.py
@@ -4,7 +4,10 @@ class Email(models.Model):
     email = models.EmailField(max_length=200)
 
 class Quizz(models.Model):
-        teamsize = models.IntegerField(null=False)
+    teamsize = models.IntegerField(null=False)
+    sprint_time = models.IntegerField(null=False)
+    company_size = models.IntegerField(null=False)
+    maturity = models.IntegerField(null=False)
 
 
 # Create your models here.
